271859d26fc44e7c7f7f3a4a925149a60cccc606
dedanirungu <dedanirungu@gmail.com>
2017-08-16 10:24:00 +0300

    Improvement to Icon setting

--- Currencies/Code/Models/CurrenciesModel.php
+++ Currencies/Code/Models/CurrenciesModel.php
@@ -60,7 +60,7 @@ class CurrenciesModel extends BaseModel {
     }
 
     function convertCurrency($amount, $from, $to) {
-        $url = "https://www.google.com/finance/converter?a=$amount&from=$from&to=$to";
+        $url = "https://www.google.com/payments/converter?a=$amount&from=$from&to=$to";
         $data = file_get_contents($url);
         preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted);
         $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
